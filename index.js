f89d4743344abfcd1578421f4c7627d934f98218
juwel36 <nothingsomthing3@gmail.com>
2023-11-05 22:34:56 +0600

    implement delete method and send data in ui

--- index.js
+++ index.js
@@ -90,6 +90,15 @@ res.send(result)
 
 })
 
+// delete
+app.delete('/create/:id',async(req,res)=>{
+const id=req.params.id
+const query={_id: new ObjectId(id) }
+const result=await assignmentsCollection.deleteOne(query)
+res.send(result)
+})
+ 
+
 
 
 
